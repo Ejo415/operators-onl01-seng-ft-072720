63515dad1f4a34afe80a1c4c4a2d6482fb062ecb
Eugene O'Neil <ejo7220@gmail.com>
2020-07-27 21:57:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,21 +2,17 @@ def unsafe?(speed)
   
   if speed < 40 
       true
-     
-     elsif speed > 60
+  elsif speed > 60
       true
-     
-     else
+  else
       false
-     
-     
-end 
+  end 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false 
 end
 	
 
